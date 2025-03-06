92949dfe2b481ae09f0ac7d6a9e324e2faa8cc61
KerrianG <kerrian.gesnel2@etu.univ-lorraine.fr>
2025-03-06 09:51:23 +0100

    Bon le polymorphisme marche pas mal c'est juste le décorateur ConjuredItem Chiantos

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -1,62 +1,39 @@
 package com.gildedrose;
 
-class GildedRose {
-    Item[] items;
+public class GildedRose {
+    final AbstractItem[] items;
 
     public GildedRose(Item[] items) {
-        this.items = items;
+        this.items = new AbstractItem[items.length];
+        for (int i = 0; i < items.length; i++) {
+            this.items[i] = wrapItem(items[i]);
+        }
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
+        for (AbstractItem item : items){
+            item.updateQuality();
         }
     }
+
+    private static AbstractItem wrapItem(Item item) {
+        AbstractItem wrappedItem;
+
+        if (item.name.equals("Aged Brie")) {
+            wrappedItem = new AgedBrie(item);
+        } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            wrappedItem = new Sulfuras(item);
+        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            wrappedItem = new BackstagePasses(item);
+        } else {
+            wrappedItem = new BaseItem(item);
+        }
+
+        if(item.name.startsWith("Conjured")){
+            wrappedItem = new ConjuredItem(wrappedItem);
+        }
+
+        return wrappedItem;
+    }
+
 }
