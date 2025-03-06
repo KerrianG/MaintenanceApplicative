fe797772891f61ef220729dc03de9f796d5144ac
KerrianG <kerrian.gesnel2@etu.univ-lorraine.fr>
2025-03-06 10:37:01 +0100

    [Trivia] Retirer méthode isPlayable()

--- TriviaGame/src/main/java/trivia/Game.java
+++ TriviaGame/src/main/java/trivia/Game.java
@@ -31,10 +31,6 @@ public class Game implements IGame {
       return "Rock Question " + index;
    }
 
-   public boolean isPlayable() {
-      return (howManyPlayers() >= 2);
-   }
-
    public boolean add(String playerName) {
       places[howManyPlayers()] = 1;
       purses[howManyPlayers()] = 0;
